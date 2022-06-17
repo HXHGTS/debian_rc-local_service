25137ed3c955ddaf1875dd3f36f15cca4f08d0ed
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2022-06-17 08:25:38 +0800

    Update install.sh

--- install/install.sh
+++ install/install.sh
@@ -4,4 +4,16 @@ echo 正在导入服务. . .
 
 curl -sSL https://hxhgts.github.io/debian_rc-local_service/service/rc-local.service > /etc/systemd/system/rc-local.service
 
+echo 正在创建默认rc.local文件. . .
+
+curl -sSL https://hxhgts.github.io/debian_rc-local_service/etc/rc.local > /etc/rc.local
+
+chmod +x /etc/rc.local
+
+echo 正在开启服务并添加到开机启动. . .
+
+systemctl enable rc-local
+
+systemctl start rc-local
+
 exit 0
